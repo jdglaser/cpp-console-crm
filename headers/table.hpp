fa96f1e8d215b6e487d4392da1fa7a9bfa51d77e
Jarred Glaser <jarred.glaser@gmail.com>
2019-12-22 12:56:33 -0600

    .gitignore fix

--- headers/table.hpp
+++
@@ -1,9 +0,0 @@
-/* table.hpp
-Table member class definitions */
-#include "table.h"
-
-// Class to add a new row to the table
-template <typename K, typename V>
-void table<K,V>::insert_row(K key, V value) {
-    hashtable.emplace(key,value);
-}
